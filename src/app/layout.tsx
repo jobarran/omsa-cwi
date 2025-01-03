9e1824ac7b126e8f12fc066a8c18d0e779fd2862
Joaquin <jbarrandeguy@obrasmetalicas.com.ar>
2025-01-03 17:37:06 -0300

    zoom input bug 9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover, maximum-scale=1" />
       </head>
       <body suppressHydrationWarning={true} className={roboto.className}>
         {children}
