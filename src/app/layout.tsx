70d5d0168deea3b87282eef1993d87a3a8a2fb35
Joaquin <jbarrandeguy@obrasmetalicas.com.ar>
2025-01-03 17:14:29 -0300

    zoom input bug 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import './globals.css'
 import { roboto } from '@/config/fonts'
 import type { Metadata } from 'next'
-import Head from 'next/head'
-
 
 export const metadata: Metadata = {
   title: 'OMSA - CWI',
   description: 'Gestion de obra',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover',
 }
 
 export default function RootLayout({
@@ -17,12 +16,9 @@ export default function RootLayout({
 
   return (
     <html lang="es">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-      </Head>
       <body suppressHydrationWarning={true} className={roboto.className}>
         {children}
-      </body>
+        </body>
     </html>
   )
 }
